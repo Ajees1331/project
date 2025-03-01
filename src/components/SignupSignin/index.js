709e7ddd15eea7e1a10816b0d04cb12168125fa2
Ajees1331 <ajisajmeer32003@gmail.com>
2025-03-01 14:48:00 +0000

    styles

--- src/components/SignupSignin/index.js
+++ src/components/SignupSignin/index.js
@@ -1,9 +1,12 @@
 import React from 'react'
-
+import "./styles.css"
 function SignupSigninComponent() {
   return (
-    <div>SignupSigninComponent</div>
+    <div className="signup-wrapper">
+      <h2 className="title">
+        Sign Up on <span style={{color:"var(--theme"}}>Financely.</span></h2>
+    </div>
   )
 }
 
-export default SignupSigninComponent
+export default SignupSigninComponent;
